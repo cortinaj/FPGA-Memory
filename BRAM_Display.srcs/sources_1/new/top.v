d4ac19618fe22ef8716794baf6cbf9985bd09df1
cortinaj <johnpaul.cortina.716@my.csun.edu>
2025-09-21 21:37:24 -0700

    Added a top module, and generated a block memory configured as ROM with 256 depth and 8 bit width.

---
+++ BRAM_Display.srcs/sources_1/new/top.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/21/2025 09:31:42 PM
+// Design Name: 
+// Module Name: top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module top(
+    input clk,
+    input [7:0] addr,
+    output [7:0] data
+    );
+endmodule
